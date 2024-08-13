ef6dafe6b85e70b8d03387402488a9bbc5fcacca
mitchell <mitchell@gici.com.au>
2024-08-13 12:29:56 +1000

    update for teams v2

--- GITeamsSetting/Program.cs
+++ GITeamsSetting/Program.cs
@@ -12,6 +12,12 @@ namespace GITeamsSetting
     {
         static void Main(string[] args)
         {
+            Teamsv1Check();
+            TeamsV2Check();
+        }
+
+        static void Teamsv1Check()
+        {
             string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string storagefile = roaming + "\\Microsoft\\Teams\\storage.json";
 
@@ -42,7 +48,41 @@ namespace GITeamsSetting
             {
                 writer.Write(storagedata);
             }
-            
+        }
+
+        static void TeamsV2Check()
+        {
+           string localAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string appSettingsFile = Path.Combine(localAppDataFolder, "Packages\\MSTeams_8wekyb3d8bbwe\\LocalCache\\Microsoft\\MSTeams\\app_settings.json");
+            if (!File.Exists(appSettingsFile))
+            {
+                Console.WriteLine("No Teams v2 app settings file");
+                return;
+            }
+
+            string storagedata = File.ReadAllText(appSettingsFile);
+            if (!storagedata.Contains("\"open_file_in_desktop_app\""))
+            {
+                //Missing setting.
+                Console.WriteLine("open_file_in_desktop_app missing - adding and setting true.");
+                storagedata = storagedata.Substring(0, storagedata.Length - 1); //trim closing brace.
+                storagedata = storagedata + ",\"open_file_in_desktop_app\":true}";
+            }
+            else if (storagedata.Contains("\"open_file_in_desktop_app\":false"))
+            {
+                //Update to true.
+                Console.WriteLine("open_file_in_desktop_app missing - setting false.");
+                storagedata = storagedata.Replace("\"open_file_in_desktop_app\":false", "\"open_file_in_desktop_app\":true");
+            }
+
+            //Console.Write(storagedata);
+            using (StreamWriter writer = new StreamWriter(appSettingsFile))
+            {
+                writer.Write(storagedata);
+            }
+            //"open_file_in_desktop_app":true
+            //C:\Users\gici\AppData\Local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\app_settings.json
+
         }
     }
 }
